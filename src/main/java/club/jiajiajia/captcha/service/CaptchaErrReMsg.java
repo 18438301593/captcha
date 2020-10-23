cf4d70010ad28c2b719e2dc2544c864e14f0957b
18438301593 <50407164+18438301593@users.noreply.github.com>
2020-10-24 00:26:37 +0800

    Add files via upload

---
+++ src/main/java/club/jiajiajia/captcha/service/CaptchaErrReMsg.java
@@ -0,0 +1,10 @@
+package club.jiajiajia.captcha.service;
+
+/**
+ * @author : JIAJIAJIA
+ * @project weblog
+ * @date : 2020-10-24 00:22
+ * @description
+ **/
+public interface CaptchaErrReMsg {
+}
